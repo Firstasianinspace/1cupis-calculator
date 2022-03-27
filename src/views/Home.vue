fe4490c1834ed99f86e6f88bfc327aa647709a69
FirstAsianInSpace <55629177+Firstasianinspace@users.noreply.github.com>
2022-03-28 07:23:20 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input
+      type="file"
+      accept=".csv"
+      class="file-input"
+      @change="onFileChange"
+    />
+    <div class="testing">
+      <div> {{ allDepositTotal }} </div>
+      <div> {{ allWithdrawsTotal }} </div>
+      <div v-if="profitTotal"> {{ profitTotal }} </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import * as Papa from 'papaparse';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    csvData: [],
+  }),
+  computed: {
+    /* eslint-disable */
+    allDeposit() {
+      if (this.csvData.length === 0) return;
+      const onlyDeposit = this.csvData.filter((word) => word.typeOfOperation === 'ВВОД')
+      // const newArray = onlyDeposit.map((s) => parseFloat(s.totalWithFee.replace(/,/g, ''))).reduce((a, b) => a + b)
+      return onlyDeposit
+    },
+    allWithdraws() {
+      if (this.csvData.length === 0) return;
+      const onlyWithdraws = this.csvData.filter((word) => word.typeOfOperation === 'ВЫВОД')
+      return onlyWithdraws
+    },
+    allDepositTotal: (vm) => vm.allDeposit?.map((s) => parseFloat(s.totalWithFee.replace(/,/g, ''))).reduce((a, b) => a + b),
+    allWithdrawsTotal: (vm) => vm.allWithdraws?.map((s) => parseFloat(s.totalWithFee.replace(/,/g, ''))).reduce((a, b) => a + b),
+    profitTotal: (vm) => vm.allWithdrawsTotal - vm.allDepositTotal,
+  },
+  methods: {
+    onFileChange() {
+      const fileInput = document.querySelector('.file-input');
+      this.parseData(fileInput, this.setData, this.translateKey);
+    },
+    parseData(file, setData, translateKey) {
+      Papa.parse(file.files[0], {
+        header: true,
+        encoding: 'ANSI',
+        complete(results) {
+          setData(results.data);
+        },
+        transformHeader(h) {
+          return translateKey(h);
+        },
+      });
+    },
+    setData(data) {
+      this.csvData = data;
+    },
+    translateKey(data) {
+      if (data === 'Тип операции') return 'typeOfOperation';
+      if (data === 'Дата') return 'date';
+      if (data === 'Сумма перевода с учетом комиссии') return 'totalWithFee';
+      if (data === 'Сумма перевода') return 'total';
+      if (data === 'Комиссия') return 'fee';
+      if (data === 'Способ оплаты') return 'typeOfPayment';
+      if (data === 'Кошелек') return 'wallet';
+      if (data === 'Идентификатор платежа') return 'paymentId';
+      if (data === 'Плательщик') return 'payer';
+      if (data === 'Получатель') return 'receiver';
+      return 'user';
+    },
   },
 };
 </script>
